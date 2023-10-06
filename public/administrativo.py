4a3e5aff4c6b091a8bd761af8d7137dc3813da56
fgonzalez <felmiquiztli@gmail.com>
2023-10-06 09:39:03 -0600

    asfasf

--- public/administrativo.py
+++ public/administrativo.py
@@ -328,10 +328,12 @@ worksheet.merge_range('N6:N8', 'M2 TOT', blue_header_format)
 
 row_count=9
 def ret_na(value):
-    if(len(value)>0):
-        return value
-    else:
-        return 'NA'
+    try:
+        x=float(value)
+    except:
+        x='NA'
+    
+    return x
 def num(value):
     if(len(str(value))>0):
         return value
@@ -358,11 +360,7 @@ for i in range(0,len(products)):
     #pesos
     worksheet.write('J'+str(row_count),str((num(products['total_weight'].values[i])+num(products['total_kg'].values[i]))/products['amount'].values[i])+'kg', blue_content)
     worksheet.write('K'+str(row_count),str((num(products['total_weight'].values[i])+num(products['total_kg'].values[i])))+'kg', blue_content)
-    print(products['amount'].values[i])
-    print(products['cost'].values[i])
-    print(num(products['total_weight'].values[i]))
-    print(num(products['total_kg'].values[i]))
-    worksheet.write('L'+str(row_count),products['amount'].values[i]*products['cost'].values[i]/(num(products['total_weight'].values[i])+num(products['total_kg'].values[i])), blue_content)
+    worksheet.write('L'+str(row_count),ret_na(products['amount'].values[i]*products['cost'].values[i]/(num(products['total_weight'].values[i])+num(products['total_kg'].values[i]))), blue_content)
 
     worksheet.write('M'+str(row_count),'NA', blue_content)
     worksheet.write('N'+str(row_count),'NA', blue_content)
