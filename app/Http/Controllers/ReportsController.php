62bcbb7efc2ccaefc02af1f3f77cbcaa23f8c367
fgonzalez <felmiquiztli@gmail.com>
2023-10-16 13:51:50 -0600

    asf

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -37,12 +37,8 @@ class ReportsController extends Controller
         if($pdf==0){
             return response()->download(public_path('storage/report/'.$report.$id.'.xlsx'));
         }else{
-         //shell_exec('cd'.$caminoalpoder.'/storage/report/;'.' localc --headless --convert-to pdf '.$report.$id.'.xlsx');
           $process2=new Process(['localc','--headless','--convert-to', 'pdf', $report.$id.'.xlsx'],$caminoalpoder.'/storage/report/');
-         //$process=new Process(['localc','--headless','--convert-to','pdf','--outdir',$caminoalpoder.'/storage/report/',$report.$id.'.xlsx'],$caminoalpoder.'/storage/report/');
-         //$process2 = new Process(["soffice", "--convert-to","pdf:calc_pdf_Export:{\"SinglePageSheets\":{\"type\":\"boolean\",\"value\":\"true\"}}",$report.$id.".xlsx "],$caminoalpoder.'/storage/report/');
-         // $process2->setTimeout(null);
-         // $process2->setIdleTimeout(null);
+         
           $process2->run();
           if (!$process2->isSuccessful()) {
              throw new ProcessFailedException($process2);
