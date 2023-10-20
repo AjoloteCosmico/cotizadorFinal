8e823603b7828ea3fe84616826d916baad3ae049
fgonzalez <felmiquiztli@gmail.com>
2023-10-20 12:26:59 -0600

    sfasf

--- public/kpis2.py
+++ public/kpis2.py
@@ -10,12 +10,19 @@ from dotenv import load_dotenv
 load_dotenv()
 #ESTE ARGUMENTO NO SE USA EN ESTE REPORTE, SERÁ 0 SIEMPRE UWU
 id=str(sys.argv[1])
-
-#configurar la conexion a la base de datos >>>>>>> 10037817f79290778a91a333cbe882be7f36de63
+#configurar la conexion a la base de datos
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_DATABASE = os.getenv('DB_DATABASE')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PORT = os.getenv('DB_PORT')
+# Conectar a DB
+cnx = mysql.connector.connect(user=DB_USERNAME,
+                              password=DB_PASSWORD,
+                              host='localhost',
+                              port=DB_PORT,
+                              database=DB_DATABASE,
+                              use_pure=False)
+
 a_color='#354F84'
 b_color='#91959E'
 
@@ -201,7 +208,57 @@ currentDateTime = datetime.datetime.now()
 date = currentDateTime.date()
 year = date.strftime("%Y")
 
+cotizaciones=pd.read_sql('select quotations.*, customers.customer,users.name from (quotations inner join customers on quotations.customer_id = customers.id) inner join users on users.id = quotations.user_id ',cnx)
 
+price_cols=['price','total_price','import','unit_price']
+aceros=pd.read_sql('select * from steels ',cnx)
+aceros.loc[aceros['caliber']=='EST 3 IN','caliber']='EST3'
+products=pd.DataFrame()
+import tablas_dict
+for i in tablas_dict.tablas:
+    # print(i)
+    #buscar en la base de datos todos los productos de esta tabla
+    #pertenecientes a la cotizacion pedida por el usuario.
+    p=pd.read_sql('select * from '+i,cnx)
+    p=p.assign(tabla=i)
+    if(('cost' not in p.columns)&(len(p)>0)):
+        if('caliber' not in p.columns):
+            #esto es en especifico por un caso en que todas kas piezas son cal 14
+            p=p.assign(caliber='14')
+        try:
+            p['caliber']=p['caliber'].str.replace('-','')
+        except:
+            print(' ')
+        # print(str(p['caliber'].values[0]))
+        costo=aceros.loc[aceros['caliber']==str(p['caliber'].values[0]),'cost'].values[0]
+        if('total_kg' in p.columns):
+            p=p.assign(cost=costo*p.total_kg)
+        if('total_weight' in p.columns):
+        
+            p=p.assign(cost=costo*p.total_weight)
+        if('weight_kg' in p.columns):
+        
+            p=p.assign(cost=costo*p.weight_kg)
+        if('weight' in p.columns):
+        
+            p=p.assign(cost=costo*p.weight)
+        if('long' in p.columns):
+        
+            p=p.assign(cost=costo*p.long)
+        # print(i)
+    products=products.append(p,ignore_index=True)
+cols_to_fill_str=['description','protector','model','sku']
+products[cols_to_fill_str]=products[cols_to_fill_str].fillna('')
+cols_kg=['weight','total_kg','total_weight','weight_kg']
+cols_m2=['m2','total_m2']
+price_cols=['price','total_price','import','unit_price']
+products[cols_kg+cols_m2]=products[cols_kg+cols_m2].fillna(0)
+
+kilos=products[cols_kg].sum(axis=1, numeric_only=True).sum()
+#trayendo informacion de materiales
+materials=pd.read_sql('select * from (materials left join price_list_screws on materials.price_list_screw_id= price_list_screws.id)left join price_lists on price_lists.id=materials.price_list_id',cnx)
+materials['type']=materials['type'].fillna('')
+#Diccionario de meses 
 worksheet = writer.sheets['Sheet1']
 #Encabezado del documento--------------
 worksheet.merge_range('B2:F2', 'REPORTE POR COTIZACION ', negro_b)
@@ -210,44 +267,163 @@ worksheet.merge_range('B4:F4', 'COSTOS ', negro_b)
 worksheet.write('H2', 'AÑO', negro_b)
 
 worksheet.write('I2', year, negro_b)
-worksheet.merge_range('J2:K3', """FECHA DEL REPORTE DD/MM/AAAA""", negro_b)
+worksheet.merge_range('J2:K3', """FECHA DEL REPORTE
+DD/MM/AAAA""", negro_b)
 
 worksheet.write('L2', date, negro_b)
 worksheet.insert_image("A1", "img/logo/logo.png",{"x_scale": 0.6, "y_scale": 0.6})
 
+#Cabecera de fechas
+espacios=' '
+for i in range(80):
+    espacios=espacios+' '
+worksheet.merge_range('B6:H6', "FECHA DEL REPORTE:"+espacios+str(date), blue_header_format)
+worksheet.merge_range('B7:E7', "PERIODO REPORTADO:", blue_header_format)
+
+
+worksheet.write('F7', "Mensual", blue_header_format)
+worksheet.write('G7', "DESDE (DD-MM-AAAA)", blue_header_format)
+worksheet.write('H7', "Hasta (DD-MM-AAAA)", blue_header_format)
+
+
 #Cabezera pricipal
-worksheet.merge_range('B6:J6', 'KPI S INGENIERIA Y COTIZACIONES', blue_header_format)
-worksheet.merge_range('B7:J7', 'RESUMEN EJECUTIVO', blue_header_format)
-worksheet.merge_range('B8:B10', 'PDA', blue_header_format)
-worksheet.merge_range('C8:C10', 'KPI', blue_header_format)
-worksheet.merge_range('D8:D10', 'DETALLE', blue_header_format)
-worksheet.merge_range('E8:E10', "VALOR", blue_header_format)
-worksheet.merge_range('F8:G10', 'VALOR', blue_header_format)
-worksheet.merge_range('H8:H10', "UNIDAD", blue_header_format)
-worksheet.merge_range('I8:I10', "INDICADOR", blue_header_format)
-worksheet.merge_range('J8:J10', "RESULTADO", blue_header_format)
+worksheet.merge_range('B10:B11', 'PDA', blue_header_format)
+worksheet.merge_range('C10:C11', 'KPI', blue_header_format)
+worksheet.merge_range('D10:D11', 'DETALLE', blue_header_format)
+worksheet.merge_range('E10:E11', "VALOR", blue_header_format)
+worksheet.merge_range('F10:F11', "UNIDAD", blue_header_format)
+worksheet.merge_range('G10:G11', "INDICADOR", blue_header_format)
+worksheet.merge_range('H10:H11', "RESULTADO", blue_header_format)
+def cociente(a,b):
+    if(b>0):
+        return a/b
+    else: 
+        return 0
+row=12
+#Indicadores globales
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'TOTAL COTIZADO EQUIVALENTE EN MONEDA NACIONAL',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),products[price_cols].sum(axis=1, numeric_only=True).sum(),blue_content)
+worksheet.write('F'+str(row),'MN',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'NUMERO TOTAL DE COTIZAICONES REALIZADAS',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),str(len(cotizaciones)),blue_content)
+worksheet.write('F'+str(row),'COTIZACIONES',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'COTIZACION PROMEDIO EN M.N. (TAMAÑO DE PROYECTO)',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),cociente(products[price_cols].sum(axis=1, numeric_only=True).sum(),len(cotizaciones)),blue_content)
+worksheet.write('F'+str(row),'MN',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'NUMERO DE CLIENTES A LOS QUE SE LES COTIZA EN EL PERIODO',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),str(len(cotizaciones['name'].unique())),blue_content)
+worksheet.write('F'+str(row),'CLIENTES',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'PROMEDIO COTIZADO POR CLIENTE',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),cociente(products[price_cols].sum(axis=1, numeric_only=True).sum(),len(cotizaciones['name'].unique())),blue_content)
+worksheet.write('F'+str(row),'MN',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'NUMERO DE COTIZADORES EMPLEADOS',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),str(len(cotizaciones['user_id'].unique())),blue_content)
+worksheet.write('F'+str(row),'COTIZADORES',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+worksheet.write('B'+str(row),str(row-11),blue_content)
+worksheet.write('C'+str(row),'PROMEDIO COTIZADO POR COTIZADOR',blue_content)
+worksheet.write('D'+str(row),'GLOBAL',blue_content)
+worksheet.write('E'+str(row),cociente(products[price_cols].sum(axis=1, numeric_only=True).sum(),len(cotizaciones['user_id'].unique())),blue_content)
+worksheet.write('F'+str(row),'MN',blue_content)
+worksheet.write('G'+str(row), '100%',blue_content)
+worksheet.write('H'+str(row),'',blue_content)
+row=row+1
+#CICLO PARA LOS TIPOS
+for i in cotizaciones['system'].unique():
+    estas_cotizaciones=cotizaciones.loc[cotizaciones['system']==i]
+    estos_productos=products.loc[products['quotation_id'].isin(estas_cotizaciones['id'].unique())]
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'TOTAL COTIZADO EQUIVALENTE EN MONEDA NACIONAL',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),estos_productos[price_cols].sum(axis=1, numeric_only=True).sum(),blue_content)
+    worksheet.write('F'+str(row),'MN',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'NUMERO TOTAL DE COTIZAICONES REALIZADAS',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),str(len(estas_cotizaciones)),blue_content)
+    worksheet.write('F'+str(row),'COTIZACIONES',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'COTIZACION PROMEDIO EN M.N. (TAMAÑO DE PROYECTO)',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),cociente(estos_productos[price_cols].sum(axis=1, numeric_only=True).sum(),len(estas_cotizaciones)),blue_content)
+    worksheet.write('F'+str(row),'MN',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'NUMERO DE CLIENTES A LOS QUE SE LES COTIZA EN EL PERIODO',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),str(len(estas_cotizaciones['name'].unique())),blue_content)
+    worksheet.write('F'+str(row),'CLIENTES',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'PROMEDIO COTIZADO POR CLIENTE',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),cociente(estos_productos[price_cols].sum(axis=1, numeric_only=True).sum(),len(estas_cotizaciones['name'].unique())),blue_content)
+    worksheet.write('F'+str(row),'MN',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'NUMERO DE COTIZADORES EMPLEADOS',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),str(len(estas_cotizaciones['user_id'].unique())),blue_content)
+    worksheet.write('F'+str(row),'COTIZADORES',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
+    worksheet.write('B'+str(row),str(row-11),blue_content)
+    worksheet.write('C'+str(row),'PROMEDIO COTIZADO POR COTIZADOR',blue_content)
+    worksheet.write('D'+str(row),i,blue_content)
+    worksheet.write('E'+str(row),cociente(estos_productos[price_cols].sum(axis=1, numeric_only=True).sum(),len(estas_cotizaciones['user_id'].unique())),blue_content)
+    worksheet.write('F'+str(row),'MN',blue_content)
+    worksheet.write('G'+str(row), '100%',blue_content)
+    worksheet.write('H'+str(row),'',blue_content)
+    row=row+1
 
-#Un FOR
-numero_De_Filas = 15
-for i in range(numero_De_Filas):
-    worksheet.write(10 + i, 1, i + 1)  # La columna "PDA" está en la segunda columna (columna B)
-
-#Trabajo Axel
-# Consulta para obtener los datos de la tabla "destinatios"
-
-
-
-worksheet.write('L8', "PERIODO REPORTADO:", blue_header_format)
-worksheet.write('L9', "Mensual", blue_header_format)
-worksheet.write('M8', "DESDE (DD-MM-AAAA)", blue_header_format)
-worksheet.write('M9', "01/01/2023", blue_header_format)
-worksheet.write('N8', "Hasta (DD-MM-AAAA)", blue_header_format)
-worksheet.write('N9', "31/31/2023", blue_header_format)
 
 #ajustar columnas
 worksheet.set_column('A:A',15)
-worksheet.set_column('D:D',20)
-worksheet.set_column('F:F',25)
+worksheet.set_column('C:C',38)
+worksheet.set_column('D:D',12)
+worksheet.set_column('E:E',25)
 worksheet.set_column('J:J',25)
 worksheet.set_column('L:L',15)
 worksheet.set_column('G:G',15)
@@ -256,7 +432,7 @@ worksheet.set_column('L:L',15)
 worksheet.set_column('M:M',90)
 worksheet.set_column('N:N',90)
 worksheet.set_column('I:N',15)
-worksheet.set_column('P:T',15)
+worksheet.set_column('F:F',20)
 
 #worksheet.set_landscape()
 worksheet.set_paper(9)
