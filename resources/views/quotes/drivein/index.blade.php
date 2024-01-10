3ef45fae5f3becbb0a14658df80b2a2addc88e64
fgonzalez <felmiquiztli@gmail.com>
2024-01-10 11:48:31 -0600

    asfasf

--- resources/views/quotes/drivein/index.blade.php
+++ resources/views/quotes/drivein/index.blade.php
@@ -48,6 +48,136 @@
                             </a>
                         </div>
                     </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Distanciadores</h5>
+                            <p class="card-text">Cotizador de Distanciadores.</p>
+                            <a href="{{ route('spacers.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Pisos</h5>
+                            <p class="card-text">Cotizador de para pisos.</p>
+                            <a href="{{ route('floors.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Refuerzos Pisos</h5>
+                            <p class="card-text">Cotizador de Refuerzos para pisos.</p>
+                            <a href="{{ route('floor_reinforcements.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Protectores</h5>
+                            <p class="card-text">Cotizador de Protectores.</p>
+                            <a href="{{ route('selectivo_protectors.index', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Especial</h5>
+                            <p class="card-text">Cotizador de Especial.</p>
+                            <a href="{{ route('selectivo_special.index', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Fletes</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_freights.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Instalación</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_installs', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Viáticos</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_quotation_travel_assignments', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <!-- <div class="col-sm-4 col-xs-12">
                     <div class="card">
