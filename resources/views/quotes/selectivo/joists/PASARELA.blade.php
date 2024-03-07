fc7f2a06fcbe8b13872b3c06ddfb584b6c6d34e9
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2024-03-07 16:54:34 -0600

    asf

---
+++ resources/views/quotes/selectivo/joists/PASARELA.blade.php
@@ -0,0 +1,210 @@
+
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo L 2.5 Calibre 14</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typel25joists_caliber14.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo L 2.5 Calibres: 10, 12 y 16</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typel25joists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo LR</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typelrjoists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Estructural</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typestructuraljoists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Silla</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typechairjoists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo C2</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typec2joists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo L 2.0 Calibre 14</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typel2joists_caliber14.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo L 2.0 Calibres: 10, 12 y 16</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typel2joists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Caja 2.0 Calibre 14</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typebox2joists_caliber14.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Caja 2.0 Calibres: 10, 12 y 16</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typebox2joists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Caja 2.5 Calibre 14</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typebox25joists_caliber14.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Caja 2.5 Calibres: 10, 12 y 16</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="{{ route('typebox25joists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tipo Sigma</h5>
+                            <p class="card-text">Cotizador de Vigas</p>
+                            <a href="" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
