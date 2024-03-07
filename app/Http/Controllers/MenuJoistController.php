a4313a019b35dc695d65e6019630b710f5645ed8
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2024-03-07 16:50:27 -0600

    dsg

--- app/Http/Controllers/MenuJoistController.php
+++ app/Http/Controllers/MenuJoistController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Quotation;
 class MenuJoistController extends Controller
 {
     public function show($id)
     {
+        $Quotation=Quotation::find($id);
+        $System=$Quotation->type;
         $Quotation_Id = $id;
-        return view('quotes.selectivo.joists.index', compact('Quotation_Id'));
+        return view('quotes.selectivo.joists.index', compact('Quotation_Id','System'));
     }
     public function drive_show($id)
     {
