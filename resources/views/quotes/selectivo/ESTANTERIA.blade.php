21a8c8e890b28fe0c023b7793491173d1bc4f578
AjoloteCosmico <felmiquiztli@gmail.com>
2024-04-03 00:26:13 -0600

    asf

---
+++ resources/views/quotes/selectivo/ESTANTERIA.blade.php
@@ -0,0 +1,165 @@
+<div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <h5 class="card-title">Entrepaño Pintado</h5>
+                            <p class="card-text">Cotizador de Entrepaño.</p>
+                            <a href="{{ route('menuframes.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <h5 class="card-title">Entrepaño Galvanizado</h5>
+                            <p class="card-text">Cotizador de Entrepaño.</p>
+                            <a href="{{ route('menuframes.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Vigas</h5>
+                            <p class="card-text">Cotizador de Respaldos.</p>
+                            <a href="{{ route('menujoists.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Angulos ranurados</h5>
+                            <p class="card-text">Cotizador de galletas.</p>
+                            <a href="{{route('pasarela_galleta.show',$Quotation_Id)}}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <h5 class="card-title">Escuadra de Refuerzo</h5>
+                            <p class="card-text">Cotizador de Escuadra.</p>
+                            <a href="{{ route('menuframes.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Fletes</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_freights.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Instalación</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_installs', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Viáticos</h5>
+                            <p class="card-text">Capturar.</p>
+                            <a href="{{ route('selectivo_quotation_travel_assignments', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Redacción</h5>
+                            <p class="card-text">Generar Cotización.</p>
+                            <a href="{{ route('quotations.show', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <h5 class="card-title">Cuestionario</h5>
+                            <p class="card-text">Imprimir Cuestionario de Ingenieria de Racks.</p>
+                            <a href="{{ route('rpt_rack_engineering', $Quotation_Id) }}" class="btn btn-primary">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>&nbsp;
+                                Entrar
+                            </a>
+                        </div>
+                    </div>
+                </div>
