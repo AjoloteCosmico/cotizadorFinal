d978cce05a44b56bd7af44b19b113cbdbd9356b1
AjoloteCosmico <felmiquiztli@gmail.com>
2024-04-14 12:57:49 -0600

    asf

---
+++ resources/views/quotes/estanteria/escuadras/index.blade.php
@@ -0,0 +1,48 @@
+@extends('adminlte::page')
+
+@section('title', 'COTIZADOR')
+
+@section('content_header')
+    <x-header-cot>Sistema de Cotización Tyrsa</x-header-cot>
+@stop
+
+@section('content')
+    <div class="container w-full bg-white p-3 rounded-xl shadow-xl">
+        <div class="row m-3">
+            {!! Form::open(['method'=>'POST','route'=>['estanteria_escuadras.store',$Quotation_Id]]) !!}
+            <input type="hidden" name="Quotation_Id" value="{{$Quotation_Id}}">
+            
+            <div  class="row bg-white p-4 shadow-lg rounded-lg">
+                <div class="col-sm-6 col-xs-12">
+                    <h2><i class="fa-solid fa-crop-simple"></i>&nbsp;ESCUADRAS DE REFUERZO PARA ESTANTERIA</h2>
+                    <span>Favor de Seleccionar y llenar los campos solicitados para realizar la cotización.</span>
+                    <div class="card-body text-center">
+                        <img src="{{asset('vendor/img/postes/logo.png')}}" class="img-thumbnail img-fluid max-h-80 rounded mx-auto d-block" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                <div class="form-group p-2">
+                        <x-jet-label value="* Cantidad" />
+                        <input type="number" name="amount" class="inputjet w-full text-xs uppercase" value="{{ old('amount') }}" />
+                        <x-jet-input-error for='amount' /><br>
+                    
+                    <div class="form-group p-2 gap-2 flex items-center">
+                        <button type="submit" class="btn btn-blue mb-2">
+                            <i class="fa-solid fa-calculator fa-xl"></i>&nbsp; Calcular
+                        </button>
+                        <a href="{{route('selectivo.show', [$Quotation_Id,'ESTANTERIA'])}}" class="btn btn-black mb-2">
+                            <i class="fa-solid fa-rotate-left fa-xl"></i>&nbsp; Cancelar
+                        </a>
+                    </div>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('js')
+    @if (session('no_existe') == 'ok')
+        <script type="text/javascript" src="{{ asset('vendor/mystylesjs/js/no_existe.js') }}"></script>
+    @endif
+@stop
