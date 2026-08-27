5d188859eeb648628715b9b9a546380c498dae64
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-08-27 17:21:37 -0600

    asf

--- resources/views/quotes/selectivo/freights/transports.blade.php
+++ resources/views/quotes/selectivo/freights/transports.blade.php
@@ -54,6 +54,7 @@
 @section('js')
     @if (session('message') == 'no_destino')
         <script>
+            console.log("no hay destino");
             Swal.fire({
                 title: 'No encontramos un registro con este destino y unidad',
                 text: 'Asegúrate de que exista en el catálogo de destinos.',
